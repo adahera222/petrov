fe9fd2e35912f4a0bdb4d25eb0e5eef9a33e9201
sonnym <michaud.sonny@gmail.com>
2013-12-14 16:19:20 -0500

    move input into model module

--- Components/Model.elm
+++ Components/Model.elm
@@ -1,11 +1,20 @@
 module Components.Model where
 
+type Input = { delta: Time }
+
+data State = StartScreen | Alive
+type Game = { state:State }
+
 (gameWidth,gameHeight) = (720,480)
 (halfWidth,halfHeight) = (360,240)
 
-data State = StartScreen | Alive
-
-type Game = { state:State }
+delta = inSeconds <~ fps 35
+input = sampleOn delta (Input <~ delta)
 
 defaultGame =
   { state = StartScreen }
+
+stepGame : Input -> Game -> Game
+stepGame {delta} ({state} as game) = game
+
+gameState = foldp stepGame defaultGame input
--- Main.elm
+++ Main.elm
@@ -3,19 +3,4 @@ import Window
 import Components.Model as Model
 import Components.Display as Display
 
--- Inputs
-
-type Input = { delta: Time }
-
-delta = inSeconds <~ fps 35
-
-input = sampleOn delta (Input <~ delta)
-
--- Updates
-
-stepGame : Input -> Model.Game -> Model.Game
-stepGame {delta} ({state} as game) = game
-
-gameState = foldp stepGame Model.defaultGame input
-
-main = lift2 Display.render Window.dimensions gameState
+main = lift2 Display.render Window.dimensions Model.gameState
