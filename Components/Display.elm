e923f17794f29b1696e0483b3261590c5abe71d9
sonnym <michaud.sonny@gmail.com>
2013-12-14 13:01:43 -0500

    organize display

--- Components/Display.elm
+++ Components/Display.elm
@@ -6,9 +6,15 @@ sceneBackground = rgb 15 15 15
 
 display : (Int, Int) -> Game -> Element
 display (w, h) {state} =
-  container w h middle <| collage gameWidth gameHeight
-    [ rect gameWidth gameHeight |> filled sceneBackground
-    , rect (gameWidth - 60) halfHeight |> (filled <| rgb 0 0 0) |> moveY (halfHeight / 2 - 30)
-    , polygon (path [(0, 0), (gameWidth - 60, 0), (gameWidth - 45, (-halfHeight / 2)), (-15, (-halfHeight / 2))])
-        |> (filled <| rgb 31 31 31) |> move (-halfWidth + 30, -(halfHeight / 2) + 100)
-    ]
+  let
+      background : Form
+      background = rect gameWidth gameHeight |> filled sceneBackground
+
+      worldMap : Form
+      worldMap = rect (gameWidth - 60) halfHeight |> (filled <| rgb 0 0 0) |> moveY (halfHeight / 2 - 30)
+
+      controlPanel : Form
+      controlPanel = polygon (path [(0, 0), (gameWidth - 60, 0), (gameWidth - 45, (-halfHeight / 2)), (-15, (-halfHeight / 2))])
+                      |> (filled <| rgb 31 31 31) |> move (-halfWidth + 30, -(halfHeight / 2) + 100)
+
+  in collage gameWidth gameHeight [background, worldMap, controlPanel] |> container w h middle
