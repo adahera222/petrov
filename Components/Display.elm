850c804712b4dddd552a5fc98e6854ac5c1df810
sonnym <michaud.sonny@gmail.com>
2013-12-14 13:21:19 -0500

    draw alarm

--- Components/Display.elm
+++ Components/Display.elm
@@ -10,6 +10,9 @@ display (w, h) {state} =
       background : Form
       background = rect gameWidth gameHeight |> filled sceneBackground
 
+      alarm : Form
+      alarm = oval 40 20 |> (filled <| rgb 98 2 2) |> moveY halfHeight
+
       worldMap : Form
       worldMap = rect (gameWidth - 60) halfHeight |> (filled <| rgb 0 0 0) |> moveY (halfHeight / 2 - 30)
 
@@ -17,4 +20,4 @@ display (w, h) {state} =
       controlPanel = polygon (path [(0, 0), (gameWidth - 60, 0), (gameWidth - 45, (-halfHeight / 2)), (-15, (-halfHeight / 2))])
                       |> (filled <| rgb 31 31 31) |> move (-halfWidth + 30, -(halfHeight / 2) + 100)
 
-  in collage gameWidth gameHeight [background, worldMap, controlPanel] |> container w h middle
+  in collage gameWidth gameHeight [background, alarm, worldMap, controlPanel] |> container w h middle
