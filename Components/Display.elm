9673e970a0c8c98dddf285508cd06bdd9530f9df
sonnym <michaud.sonny@gmail.com>
2013-12-14 17:49:14 -0500

    display countdown

--- Components/Display.elm
+++ Components/Display.elm
@@ -8,10 +8,10 @@ styleText : Color -> number -> String -> Element
 styleText fg h v = toText v |> text . Text.color fg . Text.height h . monospace
 
 render : (Int, Int) -> Model.Game -> Element
-render (w, h) {state} =
+render (w, h) {state, timer} =
   case state of
     Model.StartScreen -> renderStartScreen w h
-    Model.Alive -> renderGame w h
+    Model.Alive -> renderGame w h timer
 
 renderStartScreen : Int -> Int -> Element
 renderStartScreen w h =
@@ -27,8 +27,8 @@ renderStartScreen w h =
 
   in collage Model.gameWidth Model.gameHeight [background, welcomeText, startText] |> container w h middle
 
-renderGame : Int -> Int -> Element
-renderGame w h =
+renderGame : Int -> Int -> Int -> Element
+renderGame w h timer =
   let
       background : Form
       background = rect Model.gameWidth Model.gameHeight |> filled sceneBackground
@@ -40,7 +40,10 @@ renderGame w h =
       worldMap = rect (Model.gameWidth - 60) Model.halfHeight |> (filled <| rgb 0 0 0) |> moveY (Model.halfHeight / 2 - 30)
 
       controlPanel : Form
-      controlPanel = polygon (path [(0, 0), (Model.gameWidth - 60, 0), (Model.gameWidth - 45, (-Model.halfHeight / 2)), (-15, (-Model.halfHeight / 2))])
-                      |> (filled <| rgb 31 31 31) |> move (-Model.halfWidth + 30, -(Model.halfHeight / 2) + 100)
+      controlPanel = [ polygon (path [(0, 0), (Model.gameWidth - 60, 0), (Model.gameWidth - 45, (-Model.halfHeight / 2)), (-15, (-Model.halfHeight / 2))])
+                         |> (filled <| rgb 31 31 31) |> move (-Model.halfWidth + 30, -(Model.halfHeight / 2) + 100)
+                     , rect 30 20 |> (filled <| rgb 0 0 0) |> move (-Model.gameWidth / 4, -Model.gameHeight / 4)
+                     , styleText (rgb 0 255 0) 14 (show timer) |> toForm |> move (-Model.gameWidth / 4, -Model.gameHeight / 4)
+                     ] |> group
 
   in collage Model.gameWidth Model.gameHeight [background, alarm, worldMap, controlPanel] |> container w h middle
--- Components/Model.elm
+++ Components/Model.elm
@@ -2,22 +2,31 @@ module Components.Model where
 
 import Keyboard
 
-type Input = { space: Bool, delta: Time }
+type Input = { space: Bool, elapsed: Time }
 
 data State = StartScreen | Alive
-type Game = { state:State }
+type Game = { state: State, countDownStart: Time, timer: Int }
 
 (gameWidth,gameHeight) = (720,480)
 (halfWidth,halfHeight) = (360,240)
 
 delta = inSeconds <~ fps 35
 input = sampleOn delta (Input <~ Keyboard.space
-                               ~ delta)
+                               ~ every second)
 defaultGame =
-  { state = StartScreen }
+  { state = StartScreen
+  , countDownStart = 0
+  , timer = 60 }
 
 stepGame : Input -> Game -> Game
-stepGame {space, delta} ({state} as game) =
-  { game | state <- if state == StartScreen && space then Alive else state }
+stepGame {space, elapsed} ({state, countDownStart, timer} as game) =
+  { game | state <- if state == StartScreen && space then Alive else state
+         , countDownStart <- if state == Alive && countDownStart == 0 then elapsed else countDownStart
+         , timer <- if state == Alive && countDownStart /= 0 then stepTimer countDownStart elapsed else 60
+  }
+
+stepTimer : Time -> Time -> Int
+stepTimer start now =
+  60 - (round (inSeconds now) - round (inSeconds start))
 
 gameState = foldp stepGame defaultGame input
